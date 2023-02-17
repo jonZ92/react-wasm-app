5c231cfcdfdf40e875fe1ab4f08af9319fd7327a
jon <zjon234@gmail.com>
2023-02-17 21:01:04 +0800

    1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import { wasm } from "./wasm"
+import { test_wasm } from "./wasm"
 import ReactDOM from 'react-dom'
 import React from 'react'
 import "../index.css"
 
-
+test_wasm();
 class App extends React.Component {
     state = {}
     render() {
