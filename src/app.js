848d9ce82d5cf99fd0b7e7178c4601b2a8aaf44f
jon <zjon234@gmail.com>
2023-02-18 19:35:30 +0800

    1

--- src/app.js
+++ src/app.js
@@ -13,6 +13,29 @@ class App extends React.Component {
         userPassword: "",
     };
 
+    test_wasm = () => {
+        let send = wasm.send_example_to_js();
+
+        console.log(send);
+
+        console.log("-----");
+
+        let field = new Map();
+        field.set(0, "my name is jon");
+        let read = {
+            field1: field,
+            field2: [[], []],
+            field3: [21, 31, 3, 1232],
+        };
+
+        let receive = wasm.receive_example_from_js(read);
+        console.log(receive);
+    }
+
+
+
+
+
     usernameput = (event) => {
         this.setState({
             userName: event.target.value,
@@ -27,7 +50,7 @@ class App extends React.Component {
     getUser = () => {
         const username = this.state.userName;
         const password = this.state.userPassword;
-    
+        this.test_wasm();    
         axios.get("/app/wm/get/users").then(function (e) {
 
 
@@ -47,22 +70,3 @@ class App extends React.Component {
 ReactDOM.render(<App />, document.getElementById("root"));
 
 
-function test_wasm() {
-    let send = wasm.send_example_to_js();
-
-    console.log(send);
-
-    console.log("-----");
-
-    let field = new Map();
-    field.set(0, "my name is jon");
-    let read = {
-        field1: field,
-        field2: [[], []],
-        field3: [21, 31, 3, 1232],
-    };
-
-    let receive = wasm.receive_example_from_js(read);
-    console.log(receive);
-
-}
