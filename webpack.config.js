c81b849ab235a7797b021f46db41af75dac9eb9e
jon <zjon234@gmail.com>
2023-02-18 19:46:27 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,9 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test:/\.wasm$/,
+            },
         ],
     },
     plugins: [
